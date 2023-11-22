fcd7e961a3ac5f62cccff61249bf76d53852ea06
Lisin D. <mrlisdim@gmail.com>
2023-11-23 02:26:50 +0400

    Add lists decoding support

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <cassert>
 #include <cctype>
 #include <charconv>
 #include <cstdlib>
 #include <optional>
 #include <string>
 #include <string_view>
+#include <tuple>
 #include <vector>
 
 #include <fmt/core.h>
@@ -12,25 +14,53 @@
 
 using json = nlohmann::json;
 
-enum class EncodedValue
+enum class EncodedValueType
 {
     Integer,
     String,
+    List,
     Unknown,
 };
 
-json decode_bencoded_value(const std::string&);
-auto detect_bencoded_value_type(const std::string&) -> EncodedValue;
+struct EncodedValue
+{
+    EncodedValueType type;
+    std::string_view value;
+};
 
-auto is_encoded_integer(std::string_view) -> bool;
+const char END_SYMBOL = 'e';
+const char LIST_START_SYMBOL = 'l';
+const char INTEGER_START_SYMBOL = 'i';
+const char STRING_DELIMITER_SYMBOL = ':';
+
+auto decode_bencoded_value(const std::string& encoded_value)
+  -> std::optional<std::tuple<EncodedValue, json>>;
+
+auto extract_integer(std::string_view encoded_value)
+  -> std::optional<std::string_view>;
 json decode_integer(const std::string&);
 
-auto is_encoded_string(std::string_view) -> bool;
+auto extract_string(std::string_view encoded_value)
+  -> std::optional<std::string_view>;
 json decode_string(const std::string&);
+
+auto is_encoded_list(std::string_view) -> bool;
+auto decode_bencoded_list(std::string_view)
+  -> std::optional<std::tuple<EncodedValue, json>>;
+
+auto extract_bencoded_value(const std::string&) -> EncodedValue;
+
+auto to_integer(std::string_view) -> std::optional<long long int>;
+
+void tests();
 
 
 int main(int argc, char* argv[])
 {
+#ifdef ENABLE_TESTS
+    tests();
+#endif
+
     if (argc < 2) {
         fmt::println(stderr, "Usage {} decode <encoded_value>", argv[0]);
         return 1;
@@ -45,8 +75,15 @@ int main(int argc, char* argv[])
         }
 
         std::string encoded_value = argv[2];
-        json decoded_value = decode_bencoded_value(encoded_value);
-        fmt::println("{}", decoded_value.dump());
+
+        auto decoded = decode_bencoded_value(encoded_value);
+        if (decoded) {
+            auto [_, decoded_value] = *decoded;
+            fmt::println("{}", decoded_value.dump());
+        }
+        else {
+            fmt::print(stderr, "Error while decoding: {}", encoded_value);
+        }
     }
     else {
         fmt::print(stderr, "unknown command: ");
@@ -56,59 +93,97 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-json decode_bencoded_value(const std::string& encoded_value)
-{
-    switch (detect_bencoded_value_type(encoded_value)) {
-        case EncodedValue::String:
-            return decode_string(encoded_value);
 
-        case EncodedValue::Integer:
-            return decode_integer(encoded_value);
+/**
+ * @brief Try to decode source string and raise if it fails
+ *
+ * Returns pair of encoded value and result of decoding
+ *
+ * @param encoded_value
+ * @return std::tuple<EncodedValue, json>
+ */
+auto decode_bencoded_value(const std::string& encoded_value)
+  -> std::optional<std::tuple<EncodedValue, json>>
+{
+    auto bencoded_value = extract_bencoded_value(encoded_value);
+
+    switch (bencoded_value.type) {
+        case EncodedValueType::String:
+            // TODO: Fix std::string
+            return {
+              {bencoded_value, decode_string(std::string{bencoded_value.value})}
+            };
+
+        case EncodedValueType::Integer:
+            // TODO: Fix std::string
+            return {
+              {bencoded_value, decode_integer(std::string{bencoded_value.value})
+              }
+            };
+
+        case EncodedValueType::List:
+            return decode_bencoded_list(bencoded_value.value);
 
         default:
-            throw std::runtime_error(
-              fmt::format("Unhandled encoded value: {}", encoded_value)
-            );
+            return std::nullopt;
             break;
     }
 }
 
 /**
- * @brief Infer value type and return EncodedValue enum
+ * @brief Infer value type and return bounded encoded value string
  *
- * If type could not be determined runtime error will be raised
- *
- * @param encoded_value
- * @return EncodedValue
+ * For lists right bound can not be determined.
  */
-auto detect_bencoded_value_type(const std::string& encoded_value)
-  -> EncodedValue
+auto extract_bencoded_value(const std::string& encoded_value) -> EncodedValue
 {
-    if (is_encoded_string(encoded_value)) {
-        return EncodedValue::String;
+    if (auto value = extract_integer(encoded_value); value) {
+        return EncodedValue{.type = EncodedValueType::Integer, .value = *value};
     }
 
-    if (is_encoded_integer(encoded_value)) {
-        return EncodedValue::Integer;
+    if (auto value = extract_string(encoded_value); value) {
+        return EncodedValue{.type = EncodedValueType::String, .value = *value};
     }
 
-    return EncodedValue::Unknown;
+    if (is_encoded_list(encoded_value)) {
+        return EncodedValue{
+          .type = EncodedValueType::List, .value = encoded_value
+        };
+    }
+
+    return {EncodedValueType::Unknown};
 }
 
 /**
- * @brief Detect if encoded value could be encoded string
- *
- * @param encoded_value
- * @return true
- * @return false
+ * @brief Extract encoded string value or return nullopt
  */
-auto is_encoded_string(std::string_view encoded_value) -> bool
+auto extract_string(std::string_view encoded_value)
+  -> std::optional<std::string_view>
 {
+    // String starts with a digit
     if (not std::isdigit(encoded_value[0])) {
-        return false;
+        return std::nullopt;
     }
 
-    return true;
+    // Find for delimiter between length and data
+    auto colon_pos = encoded_value.find_first_of(STRING_DELIMITER_SYMBOL);
+    if (colon_pos == std::string::npos) {
+        return std::nullopt;
+    }
+
+    std::string_view encoded_len{encoded_value.begin(), colon_pos};
+
+    auto len = to_integer(encoded_len);
+    if (not len) {
+        return std::nullopt;
+    }
+
+    auto last_symbol_pos = colon_pos + 1 + *len;
+    if (last_symbol_pos > encoded_value.length()) {
+        return std::nullopt;
+    }
+
+    return std::string_view{encoded_value.begin(), last_symbol_pos};
 }
 
 /**
@@ -121,11 +196,11 @@ auto is_encoded_string(std::string_view encoded_value) -> bool
  */
 json decode_string(const std::string& encoded_string)
 {
-    size_t colon_index = encoded_string.find(':');
-    if (colon_index != std::string::npos) {
-        std::string number_string = encoded_string.substr(0, colon_index);
+    size_t delimiter_index = encoded_string.find(STRING_DELIMITER_SYMBOL);
+    if (delimiter_index != std::string::npos) {
+        std::string number_string = encoded_string.substr(0, delimiter_index);
         int64_t number = std::atoll(number_string.c_str());
-        std::string str = encoded_string.substr(colon_index + 1, number);
+        std::string str = encoded_string.substr(delimiter_index + 1, number);
         return json(str);
     }
     else {
@@ -133,30 +208,22 @@ json decode_string(const std::string& encoded_string)
     }
 }
 
-
 /**
- * @brief Detect if encoded value could be encoded integer
- *
- * @param encoded_string
- * @return true
- * @return false
+ * @brief Extract encoded integer value or return nullopt
  */
-auto is_encoded_integer(std::string_view encoded_string) -> bool
+auto extract_integer(std::string_view encoded_value)
+  -> std::optional<std::string_view>
 {
-    if (not encoded_string.starts_with("i")) {
-        return false;
+    if (not encoded_value.starts_with(INTEGER_START_SYMBOL)) {
+        return std::nullopt;
     }
 
-    auto pos = encoded_string.find_first_of("e", 1);
+    auto pos = encoded_value.find_first_of(END_SYMBOL);
     if (pos == std::string::npos) {
-        return false;
+        return std::nullopt;
     }
 
-    if (pos != encoded_string.length() - 1) {
-        return false;
-    }
-
-    return true;
+    return std::string_view{encoded_value.begin(), pos + 1};
 }
 
 /**
@@ -164,7 +231,6 @@ auto is_encoded_integer(std::string_view encoded_string) -> bool
  *
  * "i-123e" -> -123
  *
- * @param encoded_integer
  * @return json
  */
 json decode_integer(const std::string& encoded_integer)
@@ -188,4 +254,195 @@ json decode_integer(const std::string& encoded_integer)
     };
 
     return json(to_integer(encoded_integer_view));
+}
+
+
+/**
+ * @brief Detect if list ahead
+ */
+auto is_encoded_list(std::string_view encoded_value) -> bool
+{
+    if (not encoded_value.starts_with(LIST_START_SYMBOL)) {
+        return false;
+    }
+
+    return true;
+}
+
+
+/**
+ * @brief Decode bencoded list of bencoded values to pair of source string and
+ *        decoded json
+ *
+ * "l5:helloi52ee" -> ["hello", 52]
+ */
+auto decode_bencoded_list(std::string_view encoded_list)
+  -> std::optional<std::tuple<EncodedValue, json>>
+{
+    std::string_view remaining_encoded_list{encoded_list};
+    remaining_encoded_list.remove_prefix(1);  // rm "l" suffix
+    std::size_t encoded_list_len = 1;         // 1 is "l" suffix length
+
+    std::vector<json> list;
+
+    // TODO: Fix while true
+    while (true) {
+        auto decoded =
+          decode_bencoded_value(std::string(remaining_encoded_list));
+
+        if (decoded) {
+            auto [encoded, result] = *decoded;
+            list.push_back(result);
+
+            remaining_encoded_list.remove_prefix(encoded.value.length());
+            encoded_list_len += encoded.value.length();
+        }
+        else {
+            if (remaining_encoded_list[0] == END_SYMBOL) {
+                encoded_list_len += 1;
+                break;
+            }
+            else {
+                return std::nullopt;
+            }
+        }
+    }
+
+    std::string_view encoded_original_list{
+      encoded_list.begin(), encoded_list_len
+    };
+
+    return {
+      {EncodedValue{
+         .type = EncodedValueType::List,
+         .value = encoded_original_list,
+       },
+       json(list)}
+    };
+}
+
+
+auto to_integer(std::string_view s) -> std::optional<long long int>
+{
+    long long int value{};
+    auto result = std::from_chars(s.data(), s.data() + s.size(), value);
+
+    if (result.ec != std::errc{} or result.ptr != s.end()) {
+        return std::nullopt;
+    }
+
+    return value;
+};
+
+
+void tests()
+{
+    // extract_string
+    {
+        auto res = extract_string("3:test");
+        assert(res != std::nullopt);
+        assert(*res == std::string_view("3:tes"));
+    }
+
+    {
+        auto res = extract_string("4:test");
+        assert(res != std::nullopt);
+        assert(*res == std::string_view("4:test"));
+    }
+
+    {
+        auto res = extract_string("10:test");
+        assert(res == std::nullopt);
+    }
+
+    {
+        auto res = extract_string("10test");
+        assert(res == std::nullopt);
+    }
+
+    {
+        std::string_view encoded_string{"4:test1:a"};
+        auto res = extract_string(encoded_string);
+        assert(res != std::nullopt);
+        assert(*res == std::string_view("4:test"));
+
+        auto res2 =
+          extract_string(std::string_view(res->end(), encoded_string.end()));
+        assert(res2 != std::nullopt);
+        assert(*res2 == std::string_view("1:a"));
+    }
+
+    // extract_integer
+    {
+        auto res = extract_integer("i123e");
+        assert(res != std::nullopt);
+        assert(*res == std::string_view("i123e"));
+    }
+
+    {
+        auto res = extract_integer("i123ee");
+        assert(res != std::nullopt);
+        assert(*res == std::string_view("i123e"));
+    }
+
+    {
+        auto res = extract_integer("i123");
+        assert(res == std::nullopt);
+    }
+
+    // extract value
+    {
+        auto res = extract_bencoded_value("i123e");
+        assert(res.type == EncodedValueType::Integer);
+        assert(res.value == std::string_view("i123e"));
+    }
+
+    {
+        auto res = extract_bencoded_value("3:test");
+        assert(res.type == EncodedValueType::String);
+        assert(res.value == std::string_view("3:tes"));
+    }
+
+    {
+        auto res = extract_bencoded_value("le");
+        assert(res.type == EncodedValueType::List);
+        assert(res.value == std::string_view("le"));
+    }
+
+    {
+        auto res = extract_bencoded_value("abcx");
+        assert(res.type == EncodedValueType::Unknown);
+    }
+
+    // decode list
+    {
+        auto res = decode_bencoded_list("l2:abe");
+        assert(res != std::nullopt);
+
+        auto [src, result] = *res;
+        assert(src.value == std::string_view("l2:abe"));
+
+        auto expected_result = json(std::vector{json("ab")});
+        assert(result.dump() == expected_result.dump());
+    }
+
+    {
+        auto res = decode_bencoded_list("li123el2:abee");
+        assert(res != std::nullopt);
+
+        auto [src, result] = *res;
+        assert(src.value == std::string_view("li123el2:abee"));
+
+        auto expected_result =
+          json(std::vector{json(123), json(std::vector{json("ab")})});
+
+        assert(result.dump() == expected_result.dump());
+    }
+
+    {
+        auto res = decode_bencoded_list("l2:aasdasdbe");
+        assert(res == std::nullopt);
+    }
+
+    fmt::println("All tests passed");
 }
